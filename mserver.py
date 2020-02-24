bcbaa96f66aec3c1672353b09dd4a1dea8fd4ae5
David Urdaibay <urdaibad@tcd.ie>
2020-02-24 05:20:29 +0000

    Working on https, still, multithreading and caching seem to work

--- mserver.py
+++ mserver.py
@@ -7,7 +7,7 @@ if len(sys.argv) == 2:
 else:
     # default port
     port = 8080
-logger_file_name = "log.txt"
+logger_file_name = 'log.txt'
 
 class Mserver:
     def __init__(self):
@@ -25,12 +25,12 @@ class Mserver:
 
         # bind the socket to a local host and a port
         self.s.bind(('127.0.0.1', port))
-        # print("socket binded to {}".format(port))
+        # print('socket binded to {}'.format(port))
         # server socket allowing up to 10 client connections
         self.s.listen(7)
         message = str(datetime.datetime.now())+'\n'
         self.log_info(message)
-        message = "Host Name: Localhost\nHost address: 127.0.0.1\nHost port: "+str(port)+"\n"
+        message = 'Host Name: Localhost\nHost address: 127.0.0.1\nHost port: '+str(port)+'\n'
         print(message)
         self.log_info(message)
         print('Server is ready to listen to clients\n')
@@ -42,27 +42,27 @@ class Mserver:
 # Translate the host/port argument into a sequence of 5-tuples that contain all the necessary # arguments for creating a socket connected to that service. host is a domain name, a string #  representation of an IPv4/v6 address or None. port is a string service name such as
 # 'http', a numeric port number or None. By passing None as the value of host and port, you can pass NULL # to the underlying C API.
     def listen_to_client(self):
-        """ waiting for clients to connect over tcp to the
-        proxy server"""
+        ''' waiting for clients to connect over tcp to the
+        proxy server'''
         while True:
 
             # Establish the connection -> accept connetions from outside
             (clientSocket, client_address) = self.s.accept()
             # printing the relevant client details on the server side
-            client_details_log = "-----------the book of facts-----------\n"
-            client_details_log += "Client host name: "+str(client_address[0])+"\
-            \nClient port number: "+str(client_address[1])+"\n"
+            client_details_log = '-----------the book of facts-----------\n'
+            client_details_log += 'Client host name: '+str(client_address[0])+'\
+            \nClient port number: '+str(client_address[1])+'\n'
             client_socket_details = socket.getaddrinfo(str(client_address[0]),
             client_address[1])
             # print(client_sockocket_details)
-            client_details_log += "Socket family: "+str(client_socket_details[0][0])+"\n"
-            client_details_log += "Socket type: "+str(client_socket_details[0][1])+"\n"
-            client_details_log += "Timeout: "+str(clientSocket.gettimeout())+"\n"
-            client_details_log += "-----------------------------------------\n"
+            client_details_log += 'Socket family: '+str(client_socket_details[0][0])+'\n'
+            client_details_log += 'Socket type: '+str(client_socket_details[0][1])+'\n'
+            client_details_log += 'Timeout: '+str(clientSocket.gettimeout())+'\n'
+            client_details_log += '-----------------------------------------\n'
             self.log_info(client_details_log)
             # Logging
-            message = "Client IP address: "+str(client_address[0])+" and Client port \
-                       number: "+str(client_address[1])+"\n"
+            message = 'Client IP address: '+str(client_address[0])+' and Client port \
+                       number: '+str(client_address[1])+'\n'
             self.log_info(message)
             # creating a new thread for every client
             d = threading.Thread(name = str(client_address),
@@ -109,7 +109,7 @@ class Mserver:
                     url_connect = url_part.split('/')[0]
                 else:
                     # if the url ends with / removing it e.g www.dave.com/
-                    if http_part.split('//')[1][-1] == "/":
+                    if http_part.split('//')[1][-1] == '/':
                         url_part = http_part.split('//')[1][:-1]
                         # removing the '/'
                         print('URL: '+url_part+'\n')
@@ -118,7 +118,7 @@ class Mserver:
                     else:
                         url_part = http_part.split('//')[1]
                         print('URL: '+url_part)
-                        getting the www.dave.com part
+                        #getting the www.dave.com part
                         url_connect = url_part.split('/')[0]
                 # url_connect = url_part
                 # getting the part after the host --> TLD
@@ -130,11 +130,12 @@ class Mserver:
                         url_slash += '/'
                         url_slash += path
                 # checking if port number is provided
-                client_req_port_start = str(url_part).find(":")
+                client_req_port_start = str(url_part).find(':')
                 # if not use default port no
                 port_number = 80
-                # replacing all non alpha numeric characters wiht underscore
+                # replacing all non alpha numeric characters with underscore using regexp lib
                 url_file_name = re.sub('[^0-9a-zA-Z]+','_', url_part)
+                # no port specified
                 if client_req_port_start == -1:
                     print('You got the pass.\n')
                     pass
@@ -145,7 +146,7 @@ class Mserver:
                 client_address, start_time, url_connect, url_slash)
             else:
                 # not a GET command
-                message = "Client with port: " +str(client_address[1])+ '''generated
+                message = 'Client with port: ' +str(client_address[1])+ '''generated
                 a call other than GET: '''+ response +'\n'
                 print(message)
                 client_sock.send(b'HTTP/1.1 405 Method Not Allowed\r\n\r\n')
@@ -169,11 +170,18 @@ class Mserver:
             # getting the cached file for the url if it exists
             cached_file = open(url_file_name, 'r')
             # reading the contents of the file
-            message = 'Client with port: '+str(client_address[1])+'''\n: Cache hit occurred \
-            for the request. Reading from file \n'''
+            message = 'Client with port: '+str(client_address[1])+'\nCACHE HIT\n'
             print(message)
-            #self.log_info(message)
-            # get proxy server details....
+            self.log_info(message)
+            # get proxy server details from cache
+            server_socket_details = socket.getaddrinfo('Localhost', port_number)
+            server_details_message = '<body> Cached Server Details:- <br />'
+            server_details_message += '''Server host name: localhost <br /> Server port number: '''+str(port_number)+'<br>'
+            server_details_message += 'Socket family: '+str(server_socket_details[0][0])+ '<br>'
+            server_details_message += 'Socket type: '+str(server_socket_details[0][1])+ '<br>'
+            server_details_message += 'Socket protocol: '+str(server_socket_details[0][2]) + '<br>'
+            server_details_message += 'Timeout: '+str(client_sock.gettimeout())+'<br> </body>'
+            print(server_details_message)
             response_message = ''
             # print 'reading data line by line and appending it'
             with open(url_file_name) as f:
@@ -188,9 +196,13 @@ class Mserver:
             # sending the cached data
             client_sock.send(response_message.encode('utf8'))
             end_time = time.time()
-            message = 'Client with port: '+str(client_address[1])+'Time Elapsed(RTT): \
-            '+str(end_time - start_time)+"seconds \n"
-            # self.log_info(message)
+            message = 'Client with port: '+str(client_address[1]) +'''\nResponse Length:  '''+str(len(response_message))+ 'bytes\n'
+            print(message)
+            self.log_info(message)
+            message = 'Client with port: '+str(client_address[1])+'''\nTime Elapsed(RTT):
+            '''+str(end_time - start_time)+' seconds\n'
+            print(message)
+            self.log_info(message)
 
         except IOError as e:
             message = 'Client with port: '+str(client_address[1])+''' -> CACHE MISS\n Goin\' to have to report to the web server on this one chief \n'''
@@ -205,7 +217,7 @@ class Mserver:
             except socket_error as s_err:
                 print('Unable to create the socket. Error: {}'.format(s_err))
                 message = 'Unable to create the socket. Error: {}'.format(s_err)
-                # self.log_info(message)
+                self.log_info(message)
             try:
                 # setting time out so that after the last packet if no other packet comes socket will auto close in 2 seconds
                 proxy_sock.settimeout(2)
@@ -230,10 +242,11 @@ class Mserver:
                 server_socket_details = getaddrinfo(url_connect, port_number)
                 server_details_message = '<body> Web Server Details:- <br />'
                 server_details_message += 'Server host name: '+url_connect+''' <br /> \nServer port number:  '''+str(port_number)+'<br />\n'
-                server_details_message += "Socket family: " + str(server_socket_details[0][0]) + "<br />"
-                server_details_message += "Socket type: " + str(server_socket_details[0][1]) + "<br />"
-                server_details_message += "Socket protocol: " + str(server_socket_details[0][2]) + "<br />"
-                server_details_message += "Timeout: " + str(client_connection_socket.gettimeout()) + '''<br /> </body>\n'''
+                server_details_message += 'Socket family: ' + str(server_socket_details[0][0]) + '<br />'
+                server_details_message += 'Socket type: ' + str(server_socket_details[0][1]) + '<br />'
+                server_details_message += 'Socket protocol: ' + str(server_socket_details[0][2]) + '<br />'
+                server_details_message += 'Timeout: '+str(client_sock.gettimeout())+'<br /> </body>\n'
+                print(server_details_message)
                 web_serv_rappend = ''
                 # timeout flag incase there is a timeout after 2 seconds of waiting
                 timeout_flag = False
@@ -251,10 +264,11 @@ class Mserver:
                     else:
                         # all the data has been received
                         break
+                print('Data: '+web_serv_rappend)
                 # variable to store response to file
                 response_to_file = web_serv_rappend
                 # storing the response from the webserver to the client locally
-                #web_serv_rappend += str(server_details_message)
+                web_serv_rappend += str(server_details_message)
                 if timeout_flag:
                     # timeout ocurred
                     err_resp = 'HTTP/1.1 408 Request timeout \r\n\r\n'
@@ -262,7 +276,7 @@ class Mserver:
                     # err_resp += serv_deets
                     client_sock.send(err_resp.encode('utf8'))
                 else:
-                    # sernding the web server response back to the client
+                    # sending the web server response back to the client
                     client_sock.send(web_serv_resp.encode('utf8'))
                 end_time = time.time()
                 message = 'Client with port: '+str(client_address[1])+'''\nTime     Elapsed(RTT):           '''+str(end_time- start_time)+' seconds \n'
@@ -293,7 +307,7 @@ class Mserver:
                     occurred: '+str(e)+'\n'
                 self.log_info(message)
                 message = 'Client with port: '+str(client_address[1])+'response sent:\
-                    '+error_msg+" \n"
+                    '+error_msg+' \n'
                 self.log_info(message)
                 message = 'Client with port: '+str(client_address[1])+ 'Time Elapsed(RTT):\
                     '+str(end_time - start_time)+' seconds \n'
@@ -312,13 +326,13 @@ class Mserver:
 #    host_ip = socket.gethostbyname('www.google.com')
 #except socket.gaierror:
     # could not resolve the host
-    #print("there was an error resolving the host")
+    #print('there was an error resolving the host')
     #sys.exit()
 #print(sys.argv)
 #serv = Mserver()
 #serv.listen_to_client()
 #print(serv.s)
-if __name__ == "__main__": # if running this through the command line then run this
+if __name__ == '__main__': # if running this through the command line then run this
     # creating the instance of the server class
     server = Mserver()
     server.listen_to_client()
